e1a7e87719c8d79a16fb8757ce8587b67fef1d17
MarcoMorella <34299465+MarcoMorella@users.noreply.github.com>
2018-11-10 17:12:12 +0100

    Add files via upload
    First version of the Collider, default setup with 8 characters words and looking for collision over the last 48 bits ( or 12 hexadecimal) of the hash.

---
+++ CNS2.py
@@ -0,0 +1,39 @@
+import hashlib
+import random
+import string
+import time
+start_time = time.time()
+
+N = 8 #String Length
+M = 12 #Number of hexdigest's digit to check
+s = string.ascii_letters+string.digits
+Tries = 0 #Tries done before finishing
+dict = {}
+
+def get_random():
+    return ''.join(random.sample(s,N))
+  
+while True :
+    Tries = Tries+1
+    TryString = get_random()
+    Hashed = hashlib.sha1(TryString.encode("ascii")).hexdigest()
+
+    if Hashed[-M:] not in dict.keys() :
+        try: #Let's expect a MemoryError if we search a too big collision
+            dict[Hashed[-M:]] = TryString
+        except MemoryError:
+            print("LOG: MemoryError")
+            dict = {}
+    elif dict[Hashed[-M:]] != TryString :
+        break
+    else :
+        print("Randomly found an already used string!")
+        print("Number of tries made so far" ,Tries)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+print("Number of tries made" ,Tries)
+
+colliding = dict[Hashed[-M:]]
+collHash = hashlib.sha1(colliding.encode("ascii")).hexdigest()
+print (colliding+'\n'+collHash+'\n'+TryString+'\n'+Hashed)
+
